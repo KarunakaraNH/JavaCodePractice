732fa218c4a5b740b36c204d9c4504340c64b689
Karunakara <47439496+KarunakaraNH@users.noreply.github.com>
2023-10-06 13:55:42 +0530

    Update InnovaSolution.java

--- src/InnovaSolution.java
+++ src/InnovaSolution.java
@@ -4,21 +4,24 @@ public class InnovaSolution {
     public static void main(String[] args) {
 
 
-        int num=12321;
+        int num=12328;
 
-        int newNumber=0;
-        while(num!=0){
+        int reversedNumber = 0;
 
-            newNumber=num/10;
-            num=num%10;
+        while (num != 0) {
+            int remainder = num % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            num = num / 10;
         }
+
+       
        /* if(num==newNumber){
 Scanner sc=new Scanner(System.in);
 int num=sc.nextInt();
 
         }
        */
-        System.out.println("new number "+newNumber );
+        System.out.println("new number "+reversedNumber );
 
     }
 
